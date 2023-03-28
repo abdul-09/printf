69bbfbd71c610b4f4e17c62d798a13dcf59bc58e
Sqmait <111649479+Sqmait@users.noreply.github.com>
2023-03-28 11:11:56 +0300

    Update get_size.c

--- get_size.c
+++ get_size.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_size - etermines the size of a data structure
+ * get_size - determines the size of a data structure
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  *
