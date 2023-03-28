ce50fbcc712c7b6c451ac3b86e9e471f47b38094
Sqmait <111649479+Sqmait@users.noreply.github.com>
2023-03-28 11:24:45 +0300

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * is_printable - Evaluates if a char is printable
+ * is_printable - checks whether a given character or string can be printed
  * @c: Char to be evaluated.
  *
  * Return: 1 if c is printable, 0 otherwise
@@ -15,7 +15,7 @@ int is_printable(char c)
 }
 
 /**
- * append_hexa_code - Append ascci in hexadecimal code to buffer
+ * append_hexa_code - Appends a hexadecimal code to a buffer or string
  * @buffer: Array of chars.
  * @i: Index at which to start appending.
  * @ascii_code: ASSCI CODE.
@@ -38,7 +38,7 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 }
 
 /**
- * is_digit - Verifies if a char is a digit
+ * is_digit - 
  * @c: Char to be evaluated
  *
  * Return: 1 if c is a digit, 0 otherwise
@@ -52,7 +52,7 @@ int is_digit(char c)
 }
 
 /**
- * convert_size_number - Casts a number to the specified size
+ * convert_size_number - Converts a numerical value to a human-readable size.
  * @num: Number to be casted.
  * @size: Number indicating the type to be casted.
  *
@@ -68,8 +68,8 @@ long int convert_size_number(long int num, int size)
 	return ((int)num);
 }
 
-/**
- * convert_size_unsgnd - Casts a number to the specified size
+/** 
+ * convert_size_unsgnd - Converts an unsigned numerical value to readable size
  * @num: Number to be casted
  * @size: Number indicating the type to be casted
  *
